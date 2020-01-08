009df0d671695f2a97e0fb6e128202dd895ff9ad
Curtis Mason <cumason@bu.edu>
2020-01-08 18:27:41 -0500

    Beginning technical analysis java server

---
+++ technical-analysis-server/src/main/java/org/mason/curtis/App.java
@@ -0,0 +1,13 @@
+package org.mason.curtis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
